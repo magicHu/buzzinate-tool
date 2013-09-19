df1bf14c9c8dc52ba4533bd74b38363a7ff2e894
magic <huronghai2008@gmail.com>
2013-09-19 15:36:27 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuzzinateTool::Application.config.secret_token = 'faf6c9d42a48f80b837966da9dec19407d84fe0b4132cfa09fa3563cf24a98df0dec138ba81061c0ecee2bda7d7c4b8bd168c17fa58e00cd40a387db6515fc02'
